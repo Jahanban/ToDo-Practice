f44888dcd8686b59977ba6a8132605c9e0e9ed1b
Jahanban <jahanban@gmail.com>
2021-04-10 18:42:02 -0400

    JS in progress

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const container = document.querySelector(".container");
-let input = doucment.querySelector(".input");
-const add = document.querySelector(".add");
+document.querySelector('form').addEventListener('submit', handleSubmitForm)
+
+function handleSubmitForm(e) {
+    e.preventDefault();
+    let input = document.querySelector('input');
+    if (input.value != '') {
+        addTodo(input.value);
+        input.value = "";
+    }
+}
